a77c886238ddf0ea308e02f45776f0e59c86554d
Sal <salvadorchaveztrejo@gmail.com>
2020-06-03 19:55:01 -0700

    merged pages from 'joel' branch

---
+++ client/src/pages/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Iframe from 'react-iframe';
+
+function Home() {
+  return (
+    <div>
+   <Iframe url="https://register.vote.org/?partner=111111&campaign=free-tools"
+        width="450px"
+        height="2000px"
+        id="myId"
+        className="myClassname"
+        display="initial"
+        position="relative"/>
+    </div>
+  );
+}
+
+export default Home;
