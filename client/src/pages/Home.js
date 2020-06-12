f90d625fe58fd3ff2c51f41e350b82b80ee4351a
Joel <guevarra_joel@yahoo.com>
2020-06-11 18:35:09 -0700

    style changes and clean up

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,16 +1,63 @@
-import React from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Toast, ToastBody, ToastHeader } from 'reactstrap';
 
-function Learn() {
+const Learn = (props) => {
   return (
     <div>
-      <h1 className="text-center">hello</h1>
-      <p>
-        Lorem ipsum dolor sit amet, est ut enim consequat. Nostrum fastidii partiendo sed ne, no
-        mutat ludus aperiri mea, per in choro dolorem electram. Invidunt reprimique assueverit quo
-        ne, eruditi graecis pro ut. Usu ut diceret scaevola evertitur, appareat voluptatibus ad vel.
-      </p>
+      <div className="p-3 bg-danger my-2 rounded">
+        <Toast>
+          <ToastHeader>
+            Are You Able To Vote?
+          </ToastHeader>
+          <ToastBody>
+            Want to know if you are already a registered voter? Click <Link to="/verify">HERE</Link>
+          </ToastBody>
+        </Toast>
+      </div>
+      <div className="p-3 bg-primary my-2 rounded">
+        <Toast>
+          <ToastHeader>
+            Need to Register?
+          </ToastHeader>
+          <ToastBody>
+            So you are not a registered voter and need to do so?  If you have 2 min you can get it done <Link to="/register">HERE</Link>
+          </ToastBody>
+        </Toast>
+      </div>
+      <div className="p-3 my-2 rounded bg-docs-transparent-grid">
+        <Toast>
+          <ToastHeader>
+            When is the next elections?
+          </ToastHeader>
+          <ToastBody>
+            Ready to vote, now what?  Find out when the next election events are taking place <Link to="/elections">HERE</Link>
+          </ToastBody>
+        </Toast>
+      </div>
+      <div className="p-3 bg-danger my-2 rounded">
+        <Toast>
+          <ToastHeader>
+            Who do you vote for?
+          </ToastHeader>
+          <ToastBody>
+            Find more information about those who currently represent you <Link to="/representatives">HERE</Link>
+          </ToastBody>
+        </Toast>
+      </div>
+      <div className="p-3 bg-primary my-2 rounded">
+        <Toast>
+          <ToastHeader>
+            Yo! Go out and vote!!
+          </ToastHeader>
+          <ToastBody>
+            Now all thats left to do is find out where you can vote and it's right <Link to="/voter-information">HERE</Link>
+          </ToastBody>
+        </Toast>
+      </div>
+    
     </div>
   );
-}
+};
 
 export default Learn;
