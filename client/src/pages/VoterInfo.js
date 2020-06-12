4842b9ea09e50e8ccd599accba36ead211cd7a56
Rob Mason <robertddmason@gmail.com>
2020-06-11 21:17:20 -0700

    Basic map rendered for voting precincts

--- client/src/pages/VoterInfo.js
+++ client/src/pages/VoterInfo.js
@@ -1,12 +1,27 @@
 import React from "react";
-import VoterInfo from "../components/VoterInfo";
+import { GoogleMap, withScriptjs, withGoogleMap } from 'react-google-maps';
 
-function Where() {
+function Map() {
   return (
-    <div>
-      <VoterInfo />
-    </div>
-  );
-}
+    <GoogleMap
+            defaultZoom={10}
+            defaultCenter={{lat: 38.576939, lng: -121.494949}}
+        />
+      );
+    }
 
-export default Where;
+const WrappedMap = withScriptjs(withGoogleMap(Map));
+
+export default function App() {
+      return (
+          <div style={{ width: "100vw", height: "100vh"}}>
+              <WrappedMap
+              googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${
+                  process.env.REACT_APP_GOOGLE_KEY
+              }`}
+              loadingElement={<div style={{ height: `100%` }} />}
+              containerElement={<div style={{ height: `400px` }} />}
+              mapElement={<div style={{ height: `100%` }} />}/>
+          </div>
+      )
+  }
