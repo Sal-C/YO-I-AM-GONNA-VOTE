e46b043414c79118b03e77e4bb9ae1be73f3f08d
Sal <salvadorchaveztrejo@gmail.com>
2020-06-18 14:22:30 -0700

    rep card styling and fixed overflow

--- client/src/components/RepresentativeCard/index.js
+++ client/src/components/RepresentativeCard/index.js
@@ -4,11 +4,14 @@ import {Card, CardText, CardBody, CardLink, CardSubtitle, Row, Col} from 'reacts
   
   const RepresentativeCard = (props) => {
     return (
-      <div className="row ml-3 mt-2">
-                <div className="card mb-3 col-sm-6">
+      <div className="ml-3 mt-2">
+          <Row>
+          <Col sm="4"></Col>
+          <Col sm="4">
+                <div className="card mb-3" style={{backgroundColor:"black", border: "2px solid #1d7d74"}}>
                     <div className="row no-gutters">
                         <div className="col-md-4">
-                        <img src={props.repImg} className="card-img" alt={props.repName}></img>
+                        <img src={props.repImg} className="card-img" height="100%" alt={props.repName}></img>
                         </div>
                         <div className="col-md-8">
                         <div className="card-body">
@@ -20,7 +23,9 @@ import {Card, CardText, CardBody, CardLink, CardSubtitle, Row, Col} from 'reacts
                         </div>
                     </div>
                 </div>
-                
+          </Col>
+          <Col sm="4"></Col>
+          </Row>
         </div>
     );
   };
